c48118a5c84539d305f88ba6a8ef909ea44ad52c
Joseph Owen <josephowen@Josephs-MacBook-Pro.local>
2021-11-15 12:34:46 +0000

    Wireframe for service

---
+++ src/main/java/digital/and/bootcamp/surverycreator/SurveryCreatorApplication.java
@@ -0,0 +1,13 @@
+package digital.and.bootcamp.surverycreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveryCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveryCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digital/and/bootcamp/surverycreator/SurveryCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.and.bootcamp.surverycreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveryCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
