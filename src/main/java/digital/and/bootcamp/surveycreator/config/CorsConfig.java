5b6c72de18c2f42e124c4a662bdd7f10bf3c8ecd
Joseph Owen <josephowen@Josephs-MacBook-Pro.local>
2021-11-16 16:40:03 +0000

    Updated CORS mappings to every path

--- src/main/java/digital/and/bootcamp/surveycreator/config/CorsConfig.java
+++ src/main/java/digital/and/bootcamp/surveycreator/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry
-                        .addMapping("/*")
+                        .addMapping("/**")
                         .allowedOrigins("http://localhost:3000", "https://andsurvey.netlify.app/");
             }
         };
